70505c41c774a17e37f942ef9f69dd1e896a01cc
shino51 <shino.takeshita1124@gmail.com>
2022-07-14 14:07:44 +0200

    Updated search insert position and length of last word

--- src/main/java/LeetCode/LengthOfLastWord.java
+++ src/main/java/LeetCode/LengthOfLastWord.java
@@ -2,8 +2,7 @@ package LeetCode;
 
 public class LengthOfLastWord {
   public int lengthOfLastWord(String s) {
-    s = s.trim();
-    String[] words = s.split(" ");
+    String[] words = s.trim().split(" ");
     String lastWord = words[words.length - 1];
     return lastWord.length();
   }
--- src/main/java/LeetCode/SearchInsertPosition.java
+++ src/main/java/LeetCode/SearchInsertPosition.java
@@ -1,15 +1,12 @@
 package LeetCode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class SearchInsertPosition {
   public int searchInsert(int[] nums, int target) {
     if(nums[0] > target) return 0;
-    List<Integer> list = new ArrayList<>(Arrays.stream(nums).boxed().toList());
-    list.add(target);
-    List<Integer> addedTarget = list.stream().sorted().toList();
-    return addedTarget.indexOf(target);
+    for(int i = 0; i < nums.length; i++) {
+      if (target <= nums[i]) {
+        return i;
+      }
+    }
+    return nums.length;
   }
 }
