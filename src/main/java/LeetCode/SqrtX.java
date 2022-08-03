638559ff0fe3191da36b28842115967290418d98
shino51 <shino.takeshita1124@gmail.com>
2022-08-03 14:23:20 +0200

    Added Sqrt(X)

---
+++ src/main/java/LeetCode/SqrtX.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class SqrtX {
+  public int mySqrt(int x) {
+    if (x <= 1) {
+      return x;
+    }
+    for (int i = 1; i <= x / 2; i++) {
+      double powerValue = Math.pow(i, 2);
+      if (powerValue == x) {
+        return i;
+      } else if (powerValue > x) {
+        return i - 1;
+      }
+    }
+    return x / 2;
+  }
+}
