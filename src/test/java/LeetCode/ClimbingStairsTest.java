35f09abfac64759db1b217127d8ed12063de8988
shino51 <shino.takeshita1124@gmail.com>
2022-08-10 14:41:53 +0200

    Added Climbing Stairs

---
+++ src/test/java/LeetCode/ClimbingStairsTest.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClimbingStairsTest {
+  private final ClimbingStairs classUnderTest = new ClimbingStairs();
+
+  @Test
+  public void test1() {
+    assertThat(classUnderTest.climbStairs(0)).isEqualTo(0);
+    assertThat(classUnderTest.climbStairs(1)).isEqualTo(1);
+    assertThat(classUnderTest.climbStairs(2)).isEqualTo(2);
+    assertThat(classUnderTest.climbStairs(3)).isEqualTo(3);
+    assertThat(classUnderTest.climbStairs(4)).isEqualTo(5);
+    assertThat(classUnderTest.climbStairs(5)).isEqualTo(8);
+    assertThat(classUnderTest.climbStairs(40)).isEqualTo(165580141);
+    assertThat(classUnderTest.climbStairs(45)).isEqualTo(1836311903);
+  }
+}
