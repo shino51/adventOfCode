a7e665256e5cddc3268e2d6bfd6661a34b1c4f27
shino51 <shino.takeshita1124@gmail.com>
2023-03-01 15:11:58 +0100

    added PascalTriangle2 and SwapNodesInPairs

---
+++ src/main/java/LeetCode/SwapNodesInPairs.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+import LeetCode.Utils.ListNode;
+
+public class SwapNodesInPairs {
+
+  public ListNode swapPairs(ListNode head) {
+    swapPairsHelper(head);
+    return head;
+  }
+
+  public void swapPairsHelper(ListNode head) {
+    if (head == null || head.next == null) {
+      return;
+    }
+    ListNode nextNode = head.next;
+    // swap values
+    int temp = head.val;
+    head.val = nextNode.val;
+    nextNode.val = temp;
+    swapPairs(nextNode.next);
+  }
+}
---
+++ src/test/java/LeetCode/PascalsTriangle2Test.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PascalsTriangle2Test {
+
+  private final PascalsTriangle2 classUnderTest = new PascalsTriangle2();
+
+  @Test
+  public void testReturn1() {
+    List<Integer> result = classUnderTest.getRow(0);
+    assertThat(result.toString()).isEqualTo("[1]");
+  }
+
+  @Test
+  public void testReturn2() {
+    List<Integer> result = classUnderTest.getRow(3);
+    assertThat(result.toString()).isEqualTo("[1, 3, 3, 1]");
+  }
+
+  @Test
+  public void testReturn3() {
+    List<Integer> result = classUnderTest.getRow(10);
+    assertThat(result.toString()).isEqualTo("[1, 10, 45, 120, 210, 252, 210, 120, 45, 10, 1]");
+  }
+}
