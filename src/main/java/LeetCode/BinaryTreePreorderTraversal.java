f80969e98a952ffbb66a9f9ba94b73cc701aeaa7
shino51 <shino.takeshita1124@gmail.com>
2023-03-15 14:36:42 +0100

    Added BinaryTreePreorderTraversal

---
+++ src/main/java/LeetCode/BinaryTreePreorderTraversal.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+import LeetCode.Utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+  public List<Integer> preorderTraversal(TreeNode root) {
+    List<Integer> list = new ArrayList<>();
+    convertToListPreorder(list, root);
+    return list;
+  }
+
+  private void convertToListPreorder(List<Integer> list, TreeNode currentNode) {
+    if (currentNode == null) {
+      return;
+    }
+    list.add(currentNode.val);
+    convertToListPreorder(list, currentNode.left);
+    convertToListPreorder(list, currentNode.right);
+  }
+}
---
+++ src/test/java/LeetCode/BinaryTreePreorderTraversalTest.java
@@ -0,0 +1,52 @@
+package LeetCode;
+
+import LeetCode.Utils.TreeNode;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BinaryTreePreorderTraversalTest {
+
+  private final BinaryTreePreorderTraversal classUnderTest = new BinaryTreePreorderTraversal();
+
+  @Test
+  public void testWithOneNode() {
+    TreeNode root = new TreeNode(1);
+    List<Integer> result = classUnderTest.preorderTraversal(root);
+
+    assertThat(result).hasSize(1);
+    assertThat(result.get(0)).isEqualTo(1);
+  }
+  @Test
+  public void testWithNullNode() {
+    List<Integer> result = classUnderTest.preorderTraversal(null);
+
+    assertThat(result).hasSize(0);
+  }
+
+  @Test
+  public void testWithThreeNode() {
+    TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    List<Integer> result = classUnderTest.preorderTraversal(root);
+
+    assertThat(result).hasSize(3);
+    assertThat(result.get(0)).isEqualTo(1);
+    assertThat(result.get(1)).isEqualTo(2);
+    assertThat(result.get(2)).isEqualTo(3);
+  }
+
+  @Test
+  public void testWithComplexStructuredNode() {
+    TreeNode root = new TreeNode(1, new TreeNode(2, new TreeNode(3), null), new TreeNode(4, null, new TreeNode(5)));
+    List<Integer> result = classUnderTest.preorderTraversal(root);
+
+    assertThat(result).hasSize(5);
+    assertThat(result.get(0)).isEqualTo(1);
+    assertThat(result.get(1)).isEqualTo(2);
+    assertThat(result.get(2)).isEqualTo(3);
+    assertThat(result.get(3)).isEqualTo(4);
+    assertThat(result.get(4)).isEqualTo(5);
+  }
+}
