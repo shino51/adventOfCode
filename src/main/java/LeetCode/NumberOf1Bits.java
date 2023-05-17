4221fa9dfc65da5f4d93070f58135e5065a49ba3
shino51 <shino.takeshita1124@gmail.com>
2023-05-17 14:48:51 +0200

    Added NumberOf1Bits

---
+++ src/main/java/LeetCode/NumberOf1Bits.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+public class NumberOf1Bits {
+
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    String binary = Integer.toBinaryString(n);
+    return (int) binary.chars().filter(ch -> ch == '1').count();
+  }
+}
